deddfbbdba5ed0a24f7dadce2604dd63b60d4047
Lenner <Grayson.Lenner@quartech.com>
2023-03-17 14:16:36 -0700

    added Main component to prevent state in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import React, { useState } from "react";
-import FilterRow from "./components/FilterRow";
-import TaxonDisplay from "./components/TaxonDisplay";
+import Main from "./components/Main";
 
 const App = (): JSX.Element => {
-  //HOOKS here
-  const [selectedTaxon, setSelectedTaxon] = useState("");
-
-  //HOOK CALL BACKS here
-  const handleTaxonFromFilterRow = (taxon: string): void => {
-    setSelectedTaxon(taxon);
-  };
-
-  /*
-   * Main component Function.
-   * This is the parent component. contains all other components
-   */
-  return (
-    <div>
-      <TaxonDisplay currentTaxon={selectedTaxon} taxonID={123456} />
-      <FilterRow onTaxonSelected={handleTaxonFromFilterRow} />
-    </div>
-  );
+  return <Main />;
 };
 
 export default App;
