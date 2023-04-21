0613495b8c447ee298369a5b7bcfcb858740f180
Lenner <Grayson.Lenner@quartech.com>
2023-04-21 15:19:39 -0700

    change styles to hook styles for table

--- src/components/QuantitativeData.tsx
+++ src/components/QuantitativeData.tsx
@@ -3,9 +3,19 @@
 import React from "react";
 //IMPORT MUI packages
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { makeStyles } from "@mui/styles";
 //IMPORT Datasets+Constants
 import { IquantitativeData } from "../utils/datagrab";
 //IMPORT helper functions
+
+const useStyles = makeStyles({
+  root: {
+    height: 400,
+    width: "100%",
+    maxWidth: "75vw",
+    margin: "1rem auto",
+  },
+});
 
 /*
  *Generic props. table rows and columns
@@ -23,17 +33,18 @@ export interface MyDataGridProps<T> {
  *      measurement_name, measurement_desc, min_valu, max_value, unit
  */
 const QuantitativeData = <T extends IquantitativeData>(props: MyDataGridProps<T>) => {
+  //HOOKS
+  const classes = useStyles();
+
   //RETURN ELEMENT
   return (
-    <div style={{ height: 400, width: "100%" }}>
-      <div style={{ maxWidth: "50vw", margin: "1rem auto" }}>
-        <DataGrid
-          rows={props.rows || []}
-          columns={props.columns}
-          getRowId={(row: IquantitativeData) => row.taxon_measurement_id}
-          autoHeight
-        />
-      </div>
+    <div className={classes.root}>
+      <DataGrid
+        rows={props.rows || []}
+        columns={props.columns}
+        getRowId={(row: IquantitativeData) => row.taxon_measurement_id}
+        autoHeight
+      />
     </div>
   );
 };
