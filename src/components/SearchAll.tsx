4ccbdea7392e1b1eb9d47467bf3ff6c3e57f9828
Lenner <Grayson.Lenner@quartech.com>
2023-05-01 12:51:42 -0700

    added comments to search all

--- src/components/SearchAll.tsx
+++ src/components/SearchAll.tsx
@@ -1,15 +1,22 @@
 /** @format */
-
+//IMPORT React and Child Components
 import React, { useState } from "react";
 import { useTaxon } from "../contexts/taxonContext";
+//IMPORT MUI packages
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
 import { makeStyles } from "@mui/styles";
 import { Theme } from "@mui/material";
+//IMPORT Datasets+Constants
 import { taxonInterface } from "../utils/datagrab";
 import { classificationLevelArray } from "../utils/constants";
+//IMPORT helper functions
 import { helperGetClassificationLevel } from "../utils/helper_functions";
 
+/*
+ * STYLE definitions for useStyles hook
+ * and global theme
+ */
 const useStyles = makeStyles((globalTheme: Theme) => ({
   formControl: {
     width: "100%",
@@ -20,15 +27,30 @@ const useStyles = makeStyles((globalTheme: Theme) => ({
   },
 }));
 
+/*
+ * PARENT COMPONENT: Main.tsx
+ * Props received from parent:
+ *   dropDownTaxons:
+ *      All dropDownTaxon options within dataset
+ */
 interface FilterProps {
   dropDownTaxons: taxonInterface[];
 }
 
+/*
+ * Main component Function.
+ * This component is a basic MUI autocomplete
+ * It receives an array of taxons for the user to select from
+ */
 const SearchAll = (props: FilterProps) => {
+  //HOOKS here
   let { contextTaxon, setContextTaxon } = useTaxon();
   const [filterTaxon, setFilterTaxon] = useState<taxonInterface | null>(null);
   const classes = useStyles();
 
+  /*
+  Sorts the drop down list to have categories
+  */
   const sortedOptions = props.dropDownTaxons
     .filter((t) => t.taxon_name_latin != null)
     .sort((a, b) => {
@@ -45,13 +67,17 @@ const SearchAll = (props: FilterProps) => {
       return 0;
     });
 
+  /*
+   * Receives the new filterTaxon value selected from the drop downs
+   * gets called when a filter value changed to a different filterTaxon/null
+   */
   const handleTaxonChange = (selectedTaxon: taxonInterface | null) => {
     setFilterTaxon(selectedTaxon);
     if (selectedTaxon) {
       setContextTaxon(selectedTaxon);
     }
   };
-
+  //RETURN ELEMENT HERE
   return (
     <Autocomplete
       className={classes.selectBox}
