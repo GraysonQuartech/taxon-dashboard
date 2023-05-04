090b4ca0eff45ad0f6b59d7618893c05c5d54c20
Lenner <Grayson.Lenner@quartech.com>
2023-05-04 12:56:59 -0700

    prep work for taxon class colors

--- src/components/RowRegular.tsx
+++ src/components/RowRegular.tsx
@@ -4,7 +4,7 @@
 import React from "react";
 //IMPORT MUI packages
 import { makeStyles } from "@mui/styles";
-import { Theme } from "@mui/material";
+import { Theme, Typography } from "@mui/material";
 import { TableCell, TableRow } from "@mui/material";
 //IMPORT Datasets+Constants+Helpers
 import { IColumn } from "../utils/constants";
@@ -14,8 +14,21 @@ import { helperGetLatinNameFromID } from "../utils/helper_functions";
  */
 const useStyles = makeStyles((globalTheme: Theme) => ({
   tableCellClass: {
-    fontWeight: globalTheme.typography.fontWeightMedium + "!important",
-    height: "34px",
+    display: "flex",
+    alignItems: "center",
+  },
+  taxonCellClass: {
+    height: "50%",
+    width: "fit-content",
+    minWidth: "70px",
+    backgroundColor: "#9575cd",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    borderRadius: "70px",
+    color: `${globalTheme.palette.primary.contrastText} !important`,
+    padding: "6px",
   },
   tableCellClassDense: {
     fontWeight: globalTheme.typography.fontWeightMedium + "!important",
@@ -43,14 +56,17 @@ const TableRowRegular = <T extends Record<string, string | number | null>>(props
   //RETURN ELEMENT
   return (
     <TableRow key={props.rowID}>
-      {props.columns.map((column) => (
-        <TableCell
-          className={props.dense ? classes.tableCellClassDense : classes.tableCellClass}
-          key={column.field as string}
-        >
-          {column.field === "taxon_id"
-            ? helperGetLatinNameFromID(props.row[column.field as keyof T] as string)
-            : props.row[column.field as keyof T]}
+      {props.columns.map((column, index) => (
+        <TableCell key={column.field as string}>
+          <div
+            className={`${column.field === "taxon_id" ? classes.taxonCellClass : ""} ${
+              props.dense ? classes.tableCellClassDense : classes.tableCellClass
+            }`}
+          >
+            {column.field === "taxon_id"
+              ? helperGetLatinNameFromID(props.row[column.field as keyof T] as string)
+              : props.row[column.field as keyof T]}
+          </div>
         </TableCell>
       ))}
     </TableRow>
