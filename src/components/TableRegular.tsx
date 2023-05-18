628ccb90988a61481f676dfc8ee3fb83b204a0af
Lenner <Grayson.Lenner@quartech.com>
2023-05-18 13:04:18 -0700

    add pagination to the regular table

--- src/components/TableRegular.tsx
+++ src/components/TableRegular.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import RowRegular from "./RowRegular";
 //IMPORT MUI packages
-import { Paper, Theme } from "@mui/material";
+import { Paper, TablePagination, Theme } from "@mui/material";
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import Typography from "@mui/material/Typography";
@@ -55,6 +55,20 @@ export interface TableProps<T> {
 const RegularTable = <T extends Record<string, string | number | null>>(props: TableProps<T>) => {
   //HOOKS
   const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const startIndex = page * rowsPerPage;
+  const endIndex = startIndex + rowsPerPage;
 
   //RETURN ELEMENT
   return (
@@ -72,11 +86,26 @@ const RegularTable = <T extends Record<string, string | number | null>>(props: T
             </TableRow>
           </TableHead>
           <TableBody>
-            {props.rows.map((row) => (
-              <RowRegular row={row} columns={props.columns} rowID={props.getRowID(row)} dense={props.dense} />
+            {props.rows.slice(startIndex, endIndex).map((row) => (
+              <RowRegular
+                key={props.getRowID(row)}
+                row={row}
+                columns={props.columns}
+                rowID={props.getRowID(row)}
+                dense={props.dense}
+              />
             ))}
           </TableBody>
-        </Table>
+        </Table>{" "}
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={props.rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
       </TableContainer>
     </div>
   );
